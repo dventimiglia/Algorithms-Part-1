48416c4a52d77deb85d2a989f9a756c3099be005
David A. Ventimiglia <dventimi@gmail.com>
2017-04-20 16:45:12 -0700

    Got the project to compile

--- Week1/Percolation/src/main/java/Percolation.java
+++ Week1/Percolation/src/main/java/Percolation.java
@@ -1,9 +1,20 @@
 public class Percolation {
-    public static void main(String[] args){}
-    public Percolation(int n){}
-    public void open(int row, int col){}
-    public boolean isOpen(int row, int col){}
-    public boolean isFull(int row, int col){}
-    public int numberOfOpenSites(){}
-    public boolean percolates(){}
+    public static void main(String[] args) {
+    }
+    public Percolation(int n) {
+    }
+    public void open(int row, int col) {
+    }
+    public boolean isOpen(int row, int col) {
+	return false;
+    }
+    public boolean isFull(int row, int col) {
+	return false;
+    }
+    public int numberOfOpenSites() {
+	return -1;
+    }
+    public boolean percolates() {
+	return false;
+}
 }
--- Week1/Percolation/src/main/java/PercolationStats.java
+++ Week1/Percolation/src/main/java/PercolationStats.java
@@ -1,8 +1,18 @@
 public class PercolationStats {
-    public PercolationStats(int n, int trials){}
-    public double mean(){}
-    public double stddev(){}
-    public double confidenceLo(){}
-    public double confidenceHi(){}
-    public static void main(String[] args){}
+    public static void main(String[] args) {
+    }
+    public PercolationStats(int n, int trials) {
+    }
+    public double mean() {
+	return -1.0;
+    }
+    public double stddev() {
+	return -1.0;
+    }
+    public double confidenceLo() {
+	return -1.0;
+    }
+    public double confidenceHi() {
+	return -1.0;
+    }
 }
