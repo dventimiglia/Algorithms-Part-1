c5252e1fd4bfdcdb56b7a5a7c8aba82c356091ad
David A. Ventimiglia <dventimi@gmail.com>
2017-04-20 17:05:27 -0700

    Switched to the basic QuickFind code

--- Week1/Percolation/src/main/java/Client.java
+++ Week1/Percolation/src/main/java/Client.java
@@ -3,7 +3,7 @@ import edu.princeton.cs.algs4.*;
 public class Client {
     public static void main(String[] args) {
 	int N = StdIn.readInt();
-	UF uf = new UF(N);
+	QuickFindUF uf = new QuickFindUF(N);
 	while (!StdIn.isEmpty()) {
 	    int p = StdIn.readInt();
 	    int q = StdIn.readInt();
