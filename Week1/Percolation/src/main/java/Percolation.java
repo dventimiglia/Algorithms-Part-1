6629a694ff1badb7b0ac6b2a9cac707867362f0b
David A. Ventimiglia <dventimi@gmail.com>
2017-04-23 10:36:11 -0700

    Summary: Almost done

--- Week1/Percolation/src/main/java/Percolation.java
+++ Week1/Percolation/src/main/java/Percolation.java
@@ -1,36 +1,44 @@
 import edu.princeton.cs.algs4.*;
+import java.awt.*;
 
 public class Percolation {
-    int n;
-    boolean[] sites;
-    int nopen;
-    WeightedQuickUnionUF system;
-
     public static void main (String[] args) {
 	Percolation p = new Percolation(4);}
 
+    protected int n;
+    protected boolean[] sites;
+    protected int nopen;
+    protected WeightedQuickUnionUF system;
+    protected boolean visualize = false;
+
     public Percolation (int n) {
+	this(n, false);}
+
+    public Percolation (int n, boolean visualize) {
 	if (n<1) throw new IllegalArgumentException();
 	this.n = n;
+	this.visualize = visualize;
 	sites = new boolean[n*n+2];
 	nopen = 0;
 	system = new WeightedQuickUnionUF(n*n+2);
-	sites[0] = true;
-	sites[n+1] = true;
+	for (int i=0; i<sites.length; i++) sites[i] = false;
+	sites[top()] = true;
+	sites[bot()] = true;
 	for (int i=1; i<=n; i++) system.union(top(), index(1, i));
-	for (int i=1; i<=n; i++) system.union(index(n, i), bot());}
+	for (int i=1; i<=n; i++) system.union(index(n, i), bot());
+	if (this.visualize) {
+	    init();
+	    draw(100);}}
 
     public void open (int row, int col) {
-	sites[index(row, col)] = true;
-	nopen++;
-	join(row, col, row, col-1);
-	join(row, col, row, col+1);
-	join(row, col, row-1, col);
-	join(row, col, row+1, col);}
-
-    public void join (int row1, int col1, int row2, int col2) {
-	try {if (sites[index(row2, col2)]) system.union(index(row1, col1), index(row2, col2));}
-	catch (Exception e) {}}
+	if (!isOpen(row, col)) {
+	    sites[index(row, col)] = true;
+	    nopen++;
+	    join(row, col, row, col-1);
+	    join(row, col, row, col+1);
+	    join(row, col, row-1, col);
+	    join(row, col, row+1, col);
+	    if (visualize) draw(100);}}
 
     public boolean isOpen (int row, int col) {
 	return sites[index(row, col)];}
@@ -47,13 +55,55 @@ public class Percolation {
     public boolean isInBounds (int n) {
 	return n>=1 && n<=this.n;}
 
-    public int index (int row, int col) {
+    protected int index (int row, int col) {
 	if (!isInBounds(row) || !isInBounds(col))
 	    throw new IndexOutOfBoundsException(String.format("[%s,%s]",row,col));
-	return (row-1)*n + 1;}
+	return (row-1)*n + col;}
 
-    public int top () {
+    protected int top () {
 	return 0;}
 
-    public int bot () {
-	return n;}}
+    protected int bot () {
+	return n*n+1;}
+
+    protected void init () {
+	StdDraw.enableDoubleBuffering();}
+
+    protected void draw (int delay) {
+        StdDraw.clear();
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setXscale(-0.05*n, 1.05*n);
+        StdDraw.setYscale(-0.05*n, 1.05*n);   // leave a border to write text
+        StdDraw.filledSquare(n/2.0, n/2.0, n/2.0);
+
+        // draw n-by-n grid
+        int opened = 0;
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                if (isFull(row, col)) {
+                    StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
+                    opened++;
+                }
+                else if (isOpen(row, col)) {
+                    StdDraw.setPenColor(StdDraw.WHITE);
+                    opened++;
+                }
+                else
+                    StdDraw.setPenColor(StdDraw.BLACK);
+                StdDraw.filledSquare(col - 0.5, n - row + 0.5, 0.45);
+            }
+        }
+
+        // write status text
+        StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 12));
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.text(0.25*n, -0.025*n, numberOfOpenSites() + " open sites");
+        if (percolates()) StdDraw.text(0.75*n, -0.025*n, "percolates");
+        else StdDraw.text(0.75*n, -0.025*n, "does not percolate");
+	StdDraw.show();
+        StdDraw.pause(delay);}
+
+    protected void join (int row1, int col1, int row2, int col2) {
+	try {if (sites[index(row2, col2)]) system.union(index(row1, col1), index(row2, col2));}
+	catch (Exception e) {}}
+}
--- Week1/Percolation/src/main/java/PercolationStats.java
+++ Week1/Percolation/src/main/java/PercolationStats.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
 public class PercolationStats {
-    int n, trials;
-    double[] data;
-    double mean, stddev;
-    Random r = new Random();
-
     public static void main (String[] args) {
 	if (args.length<2) throw new IllegalArgumentException("Two arguments required.");
 	int n = Integer.parseInt(args[0]);
 	int T = Integer.parseInt(args[1]);
 	PercolationStats stats = new PercolationStats(n, T);
-	System.out.println(String.format("mean\t\t = %s", stats.mean()));
-	System.out.println(String.format("stddev\t\t = %s", stats.stddev()));
-	System.out.println(String.format("95 percent confidence interval\t = [%s, %s]", stats.confidenceLo(), stats.confidenceHi()));}
+	System.out.println(String.format("mean\t\t\t = %s", stats.mean()));
+	System.out.println(String.format("stddev\t\t\t = %s", stats.stddev()));
+	System.out.println(String.format("95%% confidence interval\t = [%s, %s]", stats.confidenceLo(), stats.confidenceHi()));}
+
+    protected int n, trials;
+    protected double[] data;
+    protected double mean, stddev;
+    protected Random r = new Random();
 
     public PercolationStats (int n, int trials) {
 	if (n<=0) throw new IllegalArgumentException(String.format("n=%s, but must be greater than 0.", n));
@@ -28,21 +28,6 @@ public class PercolationStats {
 	for (double x : data) acc2+=Math.pow(x - mean, 2);
 	stddev = Math.sqrt(acc2 / (trials - 1));}
 
-    public double runTrial () {
-	Percolation p = new Percolation(n);
-	TreeSet<Integer> openSites = new TreeSet<>();
-	for (int i=0; i<n*n; i++) openSites.add(i);
-	while (!p.percolates()) openSite(p, openSites);
-	double stat = (double)p.numberOfOpenSites() / (n*n);
-	return stat;}
-
-    public void openSite (Percolation p, TreeSet<Integer> openSites) {
-	int index = r.nextInt(n*n);
-	int col = index % n;
-	int row = index / n;
-	p.open(row+1, col+1);
-	openSites.remove(index);}
-
     public double mean () {
 	return mean;}
 
@@ -53,5 +38,20 @@ public class PercolationStats {
 	return mean - 1.96*stddev/Math.sqrt(trials);}
 
     public double confidenceHi () {
-	return mean + 1.96*stddev/Math.sqrt(trials);}}
+	return mean + 1.96*stddev/Math.sqrt(trials);}
+
+    protected double runTrial () {
+	Percolation p = new Percolation(n);
+	TreeSet<Integer> openSites = new TreeSet<>();
+	for (int i=0; i<n*n; i++) openSites.add(i);
+	while (!p.percolates()) openSite(p, openSites);
+	double stat = (double)p.numberOfOpenSites() / (n*n);
+	return stat;}
+
+    protected void openSite (Percolation p, TreeSet<Integer> openSites) {
+	int index = r.nextInt(n*n);
+	int col = index % n;
+	int row = index / n;
+	p.open(row+1, col+1);
+	openSites.remove(index);}}
 
