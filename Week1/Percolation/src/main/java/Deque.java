54602b6f89b95e60c5f1a07900b17375d35aea41
David A. Ventimiglia <dventimi@gmail.com>
2017-04-25 15:33:15 -0700

    Basic imple of Deque.  Probably broken

--- Week1/Percolation/src/main/java/Deque.java
+++ Week1/Percolation/src/main/java/Deque.java
@@ -1,2 +1,62 @@
-public class Deque {
+import java.util.*;
+
+public class Deque<T> implements Iterable<T> {
+    private class Node {
+	T item;
+	Node next;}
+
+    private Node first, last;
+
+    // public Deque () {}
+
+    public boolean isEmpty () {
+	return first==null;}
+
+    public int size () {
+	int n = 0;
+	for (T item : this) n++;
+	return n;}
+
+    public void addFirst (T item) {
+	if (item==null) throw new NullPointerException();
+
+	Node oldfirst = first;
+	first = new Node();
+	first.item = item;
+	first.next = oldfirst;}
+
+    public void addLast (T item) {
+	if (item==null) throw new NullPointerException();
+	Node oldlast = last;
+	last = new Node();
+	last.item = item;
+	last.next = null;
+	if (isEmpty()) first = last;
+	else oldlast.next = last;}
+
+    public T removeFirst () {
+	if (isEmpty()) throw new NoSuchElementException();
+	T item = first.item;
+	first = first.next;
+	return item;}
+
+    public T removeLast () {
+	if (isEmpty()) throw new NoSuchElementException();
+	T item = first.item;
+	first = first.next;
+	if (isEmpty()) last = null;
+	return item;}
+
+    public Iterator<T> iterator () {
+	return new Iterator<T>() {
+	    Node current = first;
+	    public boolean hasNext () {return current!=null;}
+	    public void remove () {throw new UnsupportedOperationException();}
+	    public T next () {
+		T item = current.item;
+		current = current.next;
+		return item;}};}
+
+    public static void main (String[] args) {
+    }
 }
