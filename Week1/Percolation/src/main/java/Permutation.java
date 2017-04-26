e462ec2e888a3b41d0464065d6b107bbae452453
David A. Ventimiglia <dventimi@gmail.com>
2017-04-26 15:38:01 -0700

    Added Permutation

--- Week1/Percolation/src/main/java/Permutation.java
+++ Week1/Percolation/src/main/java/Permutation.java
@@ -1,1 +1,9 @@
-public class Permutation {}
+import edu.princeton.cs.algs4.*;
+
+public class Permutation {
+    public static void main (String[] args) {
+	int k = Integer.parseInt(args[0]);
+	RandomizedQueue<String> q = new RandomizedQueue<>();
+	for (int i = 0; i<k; i++) q.enqueue(StdIn.readString());
+	for (String s : q) StdOut.println(s);}}
+
