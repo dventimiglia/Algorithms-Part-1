eb8536a38b218f1a355b771f89d21daa728785b3
David A. Ventimiglia <dventimi@gmail.com>
2017-04-26 11:56:00 -0700

    Getting closer

--- Week1/Percolation/src/main/java/Deque.java
+++ Week1/Percolation/src/main/java/Deque.java
@@ -4,125 +4,96 @@ public class Deque<T> implements Iterable<T> {
     private class Node {
 	T item;
 	Node next;
+	Node prev;
 	public String toString () {
-	    return String.format("[%s|%s]", item, next);}}
+	    return String.format("[%s|%s]", item, reversed ? prev : next);}}
 
-    private Node first, last, nexttolast;
+    private Node first, last;
+    private boolean reversed;
 
     public String toString () {
-	return String.format("first:  %s\nnexttolast:  %s\nlast:  %s\n", first, nexttolast, last);}
+	return String.format("%s", reversed ? last : first);}
 
     public Deque () {
 	first = null;
 	last = null;
-	nexttolast = null;}
+	reversed = false;}
 
     public boolean isEmpty () {
-	if (first==null && last==null && nexttolast==null) return true;
-	throw new IllegalStateException();}
+	return first==null && last==null;}
 
     public int size () {
 	int n = 0;
 	for (T item : this) n++;
 	return n;}
 
-    public void addFirst (T item) {
+    public void reverse () {
+	reversed = !reversed;}
+    
+    public void add (T item) {
 	if (item==null) throw new NullPointerException();
 	Node newnode = new Node();
 	newnode.item = item;
 	if (size()==0) {
 	    first = newnode;
-	    nexttolast = newnode;
 	    last = newnode;}
-	else if (size()==1) {
-	    newnode.next = first;
-	    first = newnode;
-	    nexttolast = newnode;
-	    last = last;}
-	else if (size()==2) {
-	    newnode.next = first;
-	    first = newnode;
-	    nexttolast = last;
-	    last = last;}
 	else {
-	    newnode.next = first;
-	    first = newnode;
-	    nexttolast = nexttolast;
-	    last = last;}}
+	    if (reversed) {
+		last.next = newnode;
+		newnode.prev = last;
+		last = newnode;}
+	    else {
+		first.prev = newnode;
+		newnode.next = first;
+		first = newnode;}}}
+
+    public void addFirst (T item) {
+	if (reversed) {
+	    reverse();
+	    add(item);
+	    reverse();}
+	else {
+	    add(item);}}
 
     public void addLast (T item) {
-	if (item==null) throw new NullPointerException();
-	Node newnode = new Node();
-	newnode.item = item;
-	if (size()==0) {
-	    first = newnode;
-	    nexttolast = newnode;
-	    last = newnode;}
-	if (size()==1) {
-	    newnode.next = null;
-	    first = first;
-	    nexttolast = first;
-	    first.next = newnode;
-	    last = newnode;}
-	if (size()==2) {
-	    newnode.next = null;
-	    first = first;
-	    nexttolast = last;
-	    last.next = newnode;
-	    last = newnode;}
+	if (reversed) {
+	    add(item);}
 	else {
-	    newnode.next = null;
-	    first = first;
-	    nexttolast = last;
-	    last.next = newnode;
-	    last = newnode;}}
+	    reverse();
+	    add(item);
+	    reverse();}}
+
+    public boolean isReversed () {
+	return reversed;}
 
     public T removeFirst () {
 	if (size()==0) throw new NoSuchElementException();
 	T item = first.item;
 	if (size()==1) {
-	    last = null;
-	    nexttolast = null;
-	    first = null;}
-	else if (size()==2) {
-	    last = first.next;
-	    nexttolast = first.next;
-	    first = first.next;}
+	    first = null;
+	    last = null;}
 	else {
-	    last = last;
-	    nexttolast = nexttolast;
-	    first = first.next;}
+	    if (reversed) {
+		last.prev.next = last.next;
+		last = last.prev;}
+	    else {
+		first.next.prev = first.prev;
+		first = first.next;}}
 	return item;}
 
     public T removeLast () {
 	if (size()==0) throw new NoSuchElementException();
-	T item = last.item;
-	if (size()==1) {
-	    last = null;
-	    nexttolast = null;
-	    first = null;}
-	else if (size()==2) {
-	    last = first;
-	    nexttolast = nexttolast;
-	    first.next = null;}
-	else {
-	    last = nexttolast;
-	    nexttolast.next = null;
-	    nexttolast = first;}
+	reverse();
+	T item = removeFirst();
+	reverse();
 	return item;}
 
     public Iterator<T> iterator () {
 	return new Iterator<T>() {
-	    Node current = first;
+	    Node current = reversed ? last : first;
 	    public boolean hasNext () {return current!=null;}
 	    public void remove () {throw new UnsupportedOperationException();}
 	    public T next () {
 		T item = current.item;
-		current = current.next;
-		return item;}};}
-
-    public static void main (String[] args) {
-	Deque<String> d = new Deque<>();
-	d.addFirst("1");
-    }
-}
+		current = reversed ? current.prev : current.next;
+		return item;}};}}
