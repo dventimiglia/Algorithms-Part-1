8e6117a8ae199503014709ea449071231e6a7af9
David A. Ventimiglia <dventimi@gmail.com>
2017-04-27 09:44:25 -0700

    Adding a test script

---
+++ Week1/Percolation/test.js
@@ -0,0 +1,16 @@
+var RandomizedQueue = Java.type("RandomizedQueue");
+var r = new RandomizedQueue;
+
+r.enqueue("1");
+r.enqueue("2");
+r.enqueue("3");
+r.enqueue("4");
+r.enqueue("5");
+
+for each (var e in r) print (e);
+for each (var e in r) print (e);
+for each (var e in r) print (e);
+for each (var e in r) print (e);
+for each (var e in r) print (e);
+
+
