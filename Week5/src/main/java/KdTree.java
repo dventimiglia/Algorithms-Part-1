117c6cda9ebc6c231312060d98baa500e6d816e3
David A. Ventimiglia <dventimi@gmail.com>
2017-05-16 11:34:02 -0700

    Getting ready for Week5

--- Week5/src/main/java/KdTree.java
+++ Week5/src/main/java/KdTree.java
@@ -1,10 +1,16 @@
 import edu.princeton.cs.algs4.*;
 import java.util.*;
 
-public class PointSET {
+public class KdTree {
+    private static class Node {
+	private Point2D p;
+	private RectHV rect;
+	private Node lb;
+	private Node rt;}
+
     private TreeSet<Point2D> points = new TreeSet<>();
 
-    public PointSET () {}
+    public KdTree () {}
     public boolean isEmpty () {
 	return points.isEmpty();}
     public int size () {
@@ -14,7 +20,7 @@ public class PointSET {
     public boolean contains (Point2D p) {
 	return points.contains(p);}
     public void draw () {
-	for (Point2D p : this) p.draw();}
+	for (Point2D p : points) p.draw();}
     public Iterable<Point2D> range (RectHV rect) {return null;}
     public Point2D nearest (Point2D p) {return null;}
     public static void main (String[] args) {}}
--- Week5/src/main/java/PointSET.java
+++ Week5/src/main/java/PointSET.java
@@ -1,16 +1,10 @@
 import edu.princeton.cs.algs4.*;
 import java.util.*;
 
-public class KdTree {
-    private static class Node {
-	private Point2D p;
-	private RectHV rect;
-	private Node lb;
-	private Node rt;}
-
+public class PointSET {
     private TreeSet<Point2D> points = new TreeSet<>();
 
-    public KdTree () {}
+    public PointSET () {}
     public boolean isEmpty () {
 	return points.isEmpty();}
     public int size () {
@@ -20,7 +14,13 @@ public class KdTree {
     public boolean contains (Point2D p) {
 	return points.contains(p);}
     public void draw () {
-	for (Point2D p : this) p.draw();}
+	for (Point2D p : points) p.draw();}
     public Iterable<Point2D> range (RectHV rect) {return null;}
     public Point2D nearest (Point2D p) {return null;}
-    public static void main (String[] args) {}}
+    public static void main (String[] args) {
+	StdDraw.setPenRadius(0.05);
+	StdDraw.setPenColor(StdDraw.BLUE);
+	StdDraw.point(0.5, 0.5);
+	StdDraw.setPenColor(StdDraw.MAGENTA);
+	StdDraw.line(0.2, 0.2, 0.8, 0.2);
+    }}
