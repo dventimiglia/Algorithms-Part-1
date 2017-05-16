52775f8546a24bfef2c773893906c5ec0ae9ece8
David A. Ventimiglia <dventimi@gmail.com>
2017-05-16 10:59:16 -0700

    Getting ready for Week5

--- Week5/src/main/PointSET.java
+++ Week5/src/main/PointSET.java
@@ -1,4 +1,14 @@
 import edu.princeton.cs.algs4.*;
 import java.util.*;
 
-public class PointSET {}
+public class PointSET {
+    public PointSET () {}
+    public boolean isEmpty () {}
+    public int size () {}
+    public void insert (Point2D p) {}
+    public boolean contains (Point2D p) {}
+    public void draw () {}
+    public Iterable<Point2D> range (RectHV rect) {}
+    public Point2D nearest (Point2D p) {}
+    public static void main (String[] args) {}}
+
