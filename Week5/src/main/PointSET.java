d2e7221135b3899024e60ce3ac8b358cae1156d3
David A. Ventimiglia <dventimi@gmail.com>
2017-05-16 11:17:39 -0700

    Getting ready for Week5

--- Week5/src/main/PointSET.java
+++ Week5/src/main/PointSET.java
@@ -2,13 +2,19 @@ import edu.princeton.cs.algs4.*;
 import java.util.*;
 
 public class PointSET {
-    public PointSET () {}
-    public boolean isEmpty () {}
-    public int size () {}
-    public void insert (Point2D p) {}
-    public boolean contains (Point2D p) {}
-    public void draw () {}
-    public Iterable<Point2D> range (RectHV rect) {}
-    public Point2D nearest (Point2D p) {}
-    public static void main (String[] args) {}}
+    private TreeSet<Point2D> points = new TreeSet<>();
 
+    public PointSET () {}
+    public boolean isEmpty () {
+	return points.isEmpty();}
+    public int size () {
+	return points.size();}
+    public void insert (Point2D p) {
+	points.add(p);}
+    public boolean contains (Point2D p) {
+	return points.contains(p);}
+    public void draw () {
+	for (Point2D p : this) p.draw();}
+    public Iterable<Point2D> range (RectHV rect) {return null;}
+    public Point2D nearest (Point2D p) {return null;}
+    public static void main (String[] args) {}}
