7fac35d99b99ce9851cd8e00a4e940ec7a9d9c31
David A. Ventimiglia <dventimi@gmail.com>
2017-05-16 11:46:31 -0700

    Getting ready for Week5

--- Week5/src/main/java/PointSET.java
+++ Week5/src/main/java/PointSET.java
@@ -14,13 +14,28 @@ public class PointSET {
     public boolean contains (Point2D p) {
 	return points.contains(p);}
     public void draw () {
+	StdDraw.setPenColor(StdDraw.BLACK);
+	StdDraw.setPenRadius();
+	StdDraw.square(0.5, 0.5, 0.5);
+	StdDraw.setPenRadius(0.01);
 	for (Point2D p : points) p.draw();}
     public Iterable<Point2D> range (RectHV rect) {return null;}
     public Point2D nearest (Point2D p) {return null;}
     public static void main (String[] args) {
-	StdDraw.setPenRadius(0.05);
-	StdDraw.setPenColor(StdDraw.BLUE);
-	StdDraw.point(0.5, 0.5);
-	StdDraw.setPenColor(StdDraw.MAGENTA);
-	StdDraw.line(0.2, 0.2, 0.8, 0.2);
+        String filename = args[0];
+        In in = new In(filename);
+        StdDraw.enableDoubleBuffering();
+        PointSET brute = new PointSET();
+        while (!in.isEmpty()) {
+            double x = in.readDouble();
+            double y = in.readDouble();
+            Point2D p = new Point2D(x, y);
+            brute.insert(p);}
+        double x0 = 0.0, y0 = 0.0;      // initial endpoint of rectangle
+        double x1 = 0.0, y1 = 0.0;      // current location of mouse
+        StdDraw.clear();
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+        brute.draw();
+        StdDraw.show();
     }}
